01e2c262af0c75138d82f0d3e656d0aa2e8d4bb8
Raul E. Paneque <rlraul12@gmail.com>
2024-07-04 00:08:49 +0200

    form

--- productos/urls.py
+++ productos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name= 'productos'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('formulario', views.formulario, name='formulario'),
+    path('<int:producto_id>', views.detalle, name='detalle')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render,get_object_or_404
+
+from .forms import ProductoForm
 from .models import Producto
 
 
@@ -18,3 +20,32 @@ def index(request):
         'index.html',
         context={'productos':productos}
     )
+
+def detalle(request, producto_id):
+    #try:
+    #return HttpResponse(producto_id)
+        #producto = Producto.objects.get(id=producto_id)
+        producto = get_object_or_404(Producto, id=producto_id)
+        return render(
+            request,
+            'detalle.html',
+            context={'producto':producto}
+        )
+    #except Producto.DoesNotExist:
+    #    raise Http404()
+
+def formulario(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+             form.save()
+             return HttpResponseRedirect('/productos')
+    else:
+        form = ProductoForm()
+
+
+    return render(
+        request,
+        'producto_form.html',
+        {'form':form}
+      )
